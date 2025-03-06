47603a99d92b4f131dd8660f25506f1ee4dba3e0
justinsiek <justinsiek05@gmail.com>
2025-03-06 15:38:56 -0800

    router pages

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">Login</h1>
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage;
